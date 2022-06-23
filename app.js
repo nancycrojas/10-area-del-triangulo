c0974de8ecee658b38041011b65081ee3cbca1de
nancycrojas <nanclarisaok@gmail.com>
2022-06-23 09:10:46 -0300

    Variables, datos y operadores - ejercicio 10

---
+++ app.js
@@ -0,0 +1,7 @@
+//Área de un triangulo
+
+//Crear un programa que pida al usuario ingresar el valor de la base y el valor de la altura de un triangulo, calcular su área y mostrarlo en un mensaje.
+
+const base = prompt ("Por favor ingresa el valor de la base del triángulo");
+const altura = prompt ("Por favor ingresa  el valor de la altura del triángulo");
+alert("El área del triángulo es: " + base * altura / 2);
