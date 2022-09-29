60cd2c6a1c37df545bfbfdbcf169b760cd288efb
nancycrojas <nanclarisaok@gmail.com>
2022-09-29 20:38:35 -0300

    Agregando variable y template strings

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 //Crear un programa que pida al usuario ingresar el valor de la base y el valor de la altura de un triangulo, calcular su área y mostrarlo en un mensaje.
 
-const base = prompt ("Por favor ingresa el valor de la base del triángulo");
-const altura = prompt ("Por favor ingresa  el valor de la altura del triángulo");
-alert("El área del triángulo es: " + base * altura / 2);
+let base = Number(prompt("Por favor ingresa el valor de la base del triángulo"));
+let altura = Number(prompt("Por favor ingresa  el valor de la altura del triángulo"));
+//alert("El área del triángulo es: " + base * altura / 2);
+
+
+let area = (base * altura) / 2;
+alert(`El área del triángulo con base ${base} y altura ${altura} es: ${area}`);
+
